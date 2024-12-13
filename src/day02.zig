ac9563ddf6b8165a2f95235295ed03fbd89e0ffd
Santiago Pujol <santiagopujol7@gmail.com>
2024-12-13 20:07:12 +0000

    Refactor day02

--- src/day02.zig
+++ src/day02.zig
@@ -1,75 +1,49 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const parseNumbers = @import("utils.zig").parseNumbers;
 
-const Type = enum {
-    increasing,
-    decreasing,
-};
-
-pub fn part1(input: []const u8) !u32 {
+fn valid(level: []const i8) bool {
+    var increasing = false;
+    var decreasing = false;
+    for (0..level.len - 1) |i| {
+        const diff = level[i + 1] - level[i];
+        if (diff > 0) increasing = true else decreasing = true;
+        const dis = @abs(diff);
+        if (dis <= 0 or dis > 3 or increasing and decreasing) return false;
+    }
+    return true;
+}
+pub fn part1(allocator: Allocator, input: []const u8) !u32 {
     var result: u32 = 0;
-    var it = std.mem.splitScalar(u8, input, '\n');
-    outer: while (it.next()) |line| {
-        if (line.len == 0) continue;
-        var levels = std.mem.splitScalar(u8, line, ' ');
-        const first = try std.fmt.parseInt(u32, levels.next().?, 10);
-        var tmp = try std.fmt.parseInt(u32, levels.next().?, 10);
 
-        const dis = if (first > tmp) first - tmp else tmp - first;
-        if (dis == 0 or dis > 3) {
-            continue;
-        }
-        const variation: Type = if (first < tmp) .increasing else .decreasing;
-        while (levels.next()) |level| {
-            const num = try std.fmt.parseInt(u32, level, 10);
-            if (variation == .increasing and num < tmp or variation == .decreasing and num > tmp) {
-                continue :outer;
-            }
-            const dis2 = if (num > tmp) num - tmp else tmp - num;
-            if (dis2 == 0 or dis2 > 3) {
-                continue :outer;
-            }
-            tmp = num;
-        }
-        result += 1;
+    var it = std.mem.tokenizeScalar(u8, input, '\n');
+    while (it.next()) |line| {
+        const level = try parseNumbers(i8, allocator, line, " ");
+        defer allocator.free(level);
+        if (valid(level)) result += 1;
     }
     return result;
 }
 
-fn validLevel(level: []u32) bool {
-    const variance: Type = if (level[0] < level[1]) .increasing else .decreasing;
-    for (0..level.len - 1) |i| {
-        if (variance == .increasing and level[i] > level[i + 1] or variance == .decreasing and level[i] < level[i + 1]) {
-            return false;
-        }
-        const dis = if (level[i] > level[i + 1]) level[i] - level[i + 1] else level[i + 1] - level[i];
-        if (dis == 0 or dis > 3) {
-            return false;
-        }
-    }
-    return true;
-}
-
-pub fn part2(input: []const u8) !u32 {
+pub fn part2(allocator: Allocator, input: []const u8) !u32 {
     var result: u32 = 0;
-    var it = std.mem.tokenizeScalar(u8, input, '\n');
 
+    var it = std.mem.tokenizeScalar(u8, input, '\n');
     while (it.next()) |line| {
-        var nums: [10]u32 = undefined;
-        var nums2: [10]u32 = undefined;
-        var j: u8 = 0;
-        var level = std.mem.splitScalar(u8, line, ' ');
-        while (level.next()) |n| {
-            nums[j] = try std.fmt.parseInt(u32, n, 10);
-            j += 1;
+        const level = try parseNumbers(i8, allocator, line, " ");
+        var level2 = try allocator.alloc(i8, level.len - 1);
+        defer {
+            allocator.free(level);
+            allocator.free(level2);
         }
-        if (validLevel(nums[0..j])) {
+        if (valid(level)) {
             result += 1;
             continue;
         }
-        for (0..j + 1) |n| {
-            std.mem.copyForwards(u32, &nums2, nums[0..n]);
-            std.mem.copyForwards(u32, nums2[n..], nums[n + 1 ..]);
-            if (validLevel(nums2[0 .. j - 1])) {
+        for (0..level.len) |i| {
+            @memcpy(level2[0..i], level[0..i]);
+            @memcpy(level2[i..], level[i + 1 ..]);
+            if (valid(level2)) {
                 result += 1;
                 break;
             }
@@ -87,11 +61,8 @@ pub fn main() !void {
 
     const input = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
 
-    const part1_result = try part1(input);
-    std.debug.print("Part1 result is {}\n", .{part1_result});
-
-    const part2_result = try part2(input);
-    std.debug.print("Part2 result is {}\n", .{part2_result});
+    std.debug.print("Part1 result is {}\n", .{try part1(allocator, input)});
+    std.debug.print("Part2 result is {}\n", .{try part2(allocator, input)});
 }
 
 const test_input =
@@ -104,8 +75,8 @@ const test_input =
 ;
 
 test "Part1 test" {
-    try std.testing.expect(try part1(test_input) == 2);
+    try std.testing.expect(try part1(std.testing.allocator, test_input) == 2);
 }
 test "Part2 test" {
-    try std.testing.expect(try part2(test_input) == 4);
+    try std.testing.expect(try part2(std.testing.allocator, test_input) == 4);
 }
